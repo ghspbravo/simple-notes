ea0b9a1cdd5d2e76a265a79ef3e585e18c52df14
eug.kabanov <gmspbravo@gmail.com>
2018-04-06 19:57:20 +0500

    link components with actual data from fake server

--- src/App.js
+++ src/App.js
@@ -1,69 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import noteImage from './res/sticky-note.png';
 import addImage from './res/add.png';
-
-const noteSize = {
-    width: '250px',
-    height: '250px',
-}
-
+import serverData from './fakeServer.js';
 
 const noteStyle = {
     display: 'block',
     backgroundImage: `url(${noteImage})`,
     backgroundSize: 'cover',
-    marginRight: '1.5em',
-    marginBottom: '2em',
-    ...noteSize,
 }
 
 const addNoteStyle = {
-    display: 'block',
+    textAlign: 'center',
     backgroundImage: `url(${addImage})`,
     backgroundSize: '30%',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
     border: '0.1em solid gray',
-    ...noteSize,
-    marginBottom: '2em'
 }
 
 const inputStyle = {
+    width: '100%',
     padding: '0.5em 1em',
     margin: '0.1em 0',
     backgroundColor: 'rgba(255,255,255,0.3)',
-    border: '0.1em solid gray'
+    border: '0.1em solid gray',
+    boxSizing: 'border-box'
 }
 
-const App = props => (
-    <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to Simple notes!</h1>
-        <div style={{ display: 'flex', flexWrap: 'wrap', marginLeft: '2em', marginRight: '2em' }}>
-            {Notes()}
-            {NoteEdit()}
-            {NoteAdd()}
-        </div>
-    </div>
-)
+class App extends Component {
+    constructor() {
+        super()
+        this.state = { fakeServer: {} }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ fakeServer: serverData });
+        }, 2000);
+    }
+    render() {
+        return (
+            <div>
+                {this.state.fakeServer.user ?
+                    <div>
+                        <h1 style={{ textAlign: 'center' }}>Welcome, {this.state.fakeServer.user.name}, here're your notes!</h1>
+                        <div style={{ display: 'flex', flexWrap: 'wrap', marginLeft: '2em', marginRight: '2em' }}>
+                            {
+                                this.state.fakeServer.user.notes.map(note => Note({title: note.title, text: note.text}))
+                            }
+                            {NoteEdit()}
+                            {NoteAdd()}
+                        </div>
+                    </div> : <h1 style={{ position: 'absolute', top: '48%', left: '45%', }}>Loading...</h1>
+                }
+            </div>
+        )
+    }
+}
 
-const Notes = param => (
-    <div style={noteStyle}>
-        <h2>param.title</h2>
-        <div>param.text</div>
+const Note = param => (
+    <div className="note" style={noteStyle}>
+        <h2>{param.title}</h2>
+        <div>{param.text}</div>
     </div>
 )
 
 const NoteAdd = params => (
-    <div style={addNoteStyle}>
-        <span style={{display: 'block', marginTop: '2.5em', color: 'gray',}}>you can add note here</span>
+    <div className="note" style={addNoteStyle}>
+        <span style={{ display: 'block', marginTop: '2.5em', color: 'gray', }}>you can add note here</span>
     </div>
 )
 
 const NoteEdit = params => (
-    <div style={noteStyle}>
-        <h2><input style={inputStyle} type="text" placeholder="title"/></h2>
-        <div><textarea style={inputStyle} type="text" placeholder="text"/></div>
+    <div className="note" style={noteStyle}>
+        <h2><input style={inputStyle} type="text" placeholder="title" /></h2>
+        <div><textarea style={inputStyle} type="text" placeholder="text" /></div>
     </div>
 )
 
---
+++ src/fakeServer.js
@@ -0,0 +1,29 @@
+const serverData = {
+    user: {
+        name: "Evgeny",
+        notes: [
+            {
+                title: "my first note",
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, minus."
+            },
+            {
+                title: "Lorem, ipsum.",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint, laboriosam."
+            },
+            {
+                title: "Lorem, ipsum dolor.",
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, explicabo vitae laudantium quas deserunt modi."
+            },
+            {
+                title: "Lorem ipsum dolor sit.",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, officiis!"
+            },
+            {
+                title: "my last note",
+                text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, consequatur?"
+            }
+        ]
+    }
+}
+
+export default serverData
